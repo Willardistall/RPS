b18f69312344fd004da5ec6af9682c57966a7c10
Willard Cron <willardcron@gmail.com>
2019-05-29 09:25:31 -0600

    sprite image

--- index.js
+++ index.js
@@ -6,6 +6,9 @@ console.log(choices);
 for(var i = 0; i < choices.length; i++) {
   choices[i].addEventListener("click", function() {
     userChoice = this.id;
+  compChoice = options[Math.floor(Math.random() * options.length)];
+  document.getElementById("player").innerHTML = userChoice
+  document.getElementById("opponent").innerHTML = compChoice
     // get the comp choices
     // display choices
     //figure out winner
